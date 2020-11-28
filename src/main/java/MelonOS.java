6f1e6a01b58094da155b0f807daa3a149bcf0ac2
ChaosMelone9 <christoph.kohnen1@gmail.com>
2020-11-28 12:01:19 +0100

    Initial Setup

---
+++ src/main/java/MelonOS.java
@@ -0,0 +1,13 @@
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MelonOS extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        // TODO Insert logic to be performed when the plugin is enabled
+    }
+
+    @Override
+    public void onDisable() {
+        // TODO Insert logic to be performed when the plugin is disabled
+    }
+}
